29c149a6411f9705fc956b704d3213a8434c1c1c
Sagar Patke <sagarpatke@gmail.com>
2016-10-28 23:02:20 +0530

    Created serve task in npm.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/app',
+  output: {
+    path: path.join(__dirname,'client','assets'),
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['','.jsx','.js','/index','/index.jsx','/index.js']
+  }
+}
